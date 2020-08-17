6f2b05189bf807c8e2e2c2d7272d3c997207ad9e
Jeff Chang <jeff.y.chang@gmail.com>
2020-08-17 16:38:29 -0700

    - Added CharmPy IDE environment related config files. - Added draft version of cateogrized vendors for testing in categoryDB.csv.

--- Scripts/main.py
+++ Scripts/main.py
@@ -3,13 +3,19 @@ import CategoryDB as db
 
 #GetTxVendors
 txVendors = tx.GetTxVendors("", "..\Data\Year To Date.csv")
+print("Found {} vendor(s) in this csv".format(len(txVendors)))
+input("Press any key to continue...")
 #tx.PrintTxVendors(txVendors)
 
 #FetchCategoryDB
 categoryDb = db.FetchCategoryDB("..\Data\categoryDB.csv")
+print("Foud {} categories in DB.".format(len(categoryDb)))
+input("Press any key to continue...")
 
 #db.PrintCategory(categoryDb)
 
-db.UpdateWithVendors(txVendors, categoryDb)
 
-db.SaveDb("..\Data\categoryDB.csv", categoryDb)
+
+
+#db.UpdateWithVendors(txVendors, categoryDb)
+#db.SaveDb("..\Data\categoryDB.csv", categoryDb)
